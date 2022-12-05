5b0ca8a3b168d823002f74891c4666f1bffa02d6
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-05 21:44:12 +0700

    / add code commit task27

---
+++ task27/Program.cs
@@ -0,0 +1,12 @@
+﻿// task27 программа принимает на вход любое число и выдает сумму цифр числа
+// например: 452 ->11, 81 ->10; 9012 ->12.
+Console.Clear();
+Console.WriteLine("введите число: ");
+int a = Convert.ToInt32(Console.ReadLine()), result = 0;
+while (a > 0) 
+{
+    int b = a % 10;
+    result = result + b;
+    a = a / 10;
+}
+Console.WriteLine(result);
