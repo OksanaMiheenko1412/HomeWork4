037c13a4d0cc7f9dcb4681426c4707ba2874c644
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-06 08:36:19 +0700

    / add code commit task25

---
+++ task25/Program.cs
@@ -0,0 +1,11 @@
+﻿// task25 программа принимает на вход 2 числа (A и B)и возводит A в 
+// натуральную степень B. Не использовать Math.Pow 
+
+Console.Clear();
+Console.Write("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите второе число ");
+int B = Convert.ToInt32(Console.ReadLine()), result = 1; 
+for (int i = 1; i <= B; i++)
+    result = result * A;
+Console.WriteLine($"Результат: {result}");
